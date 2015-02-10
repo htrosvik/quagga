7d0f662aa151a734d1110a9045158bf77dafe89d
Hege <hege@usit.uio.no>
2015-02-10 21:16:24 +0100

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,88 @@
+#=== Class: quagga
+# Description TBD
+# Parameters - tbd
+#
+# === Examples
+# TBD (see a pattern?)
+#
+# === Authors
+# Leslie Carr <leslie@cumulusnetworks.com>
+#
+# === License
+# Apache v2
+
+class quagga (
+  $zebra = true,
+  $ospfd = false,
+  $bgpd   = false,
+  $ospfd  = false,
+  $ospf6d = false,
+  $ripd   = false,
+  $ripngd = false,
+  $isisd  = false,
+  $babeld = false,
+) {
+  
+  include quagga::params
+  
+  package { 'quagga':
+    ensure   => present,
+    before   => Service['quagga']
+  }
+
+  service { 'quagga':
+    ensure      => running,
+    hasrestart  => true,
+    hasstatus   => false,
+    enable      => true,
+  }
+
+  file { '/etc/quagga/daemons':
+    mode    => '0644',
+    owner   => 'quagga',
+    group   => 'quagga',
+    content => template('quagga/daemons.erb'),
+    notify  => Service['quagga'],
+  }
+
+  file { '/etc/quagga/vtysh.conf':
+    mode    => '0644',
+    owner   => 'quagga',
+    group   => 'quagga',
+    content => template('quagga/vtysh.conf.erb'),
+    notify  => Service['quagga'],
+  }
+
+  if $zebra == true {
+    include quagga::zebra
+  }
+
+  if $bgpd == true {
+    include quagga::bgpd
+  }
+
+  if $ospfd == true {
+    include quagga::ospfd
+  }
+
+  if $ospf6d == true {
+    include quagga::ospf6d
+  }
+
+  if $ripd == true {
+    include quagga::ripd
+  }
+
+  if $ripngd == true {
+    include quagga::ripngd
+  }
+
+  if $isisd == true {
+    include quagga::isisd
+  }
+
+  if $babeld == true {
+    include quagga::babeld
+  }
+
+}
---
+++ manifests/params.pp
@@ -0,0 +1,38 @@
+# 
+# For now all configurable variables go here,
+# might later move to hiera optionally
+#
+class quagga::params {
+
+  $hostname = 'hege-router'
+  $password = 'sky'
+  $enable = 'enasky'
+  $router_id = $::network_lo
+
+  # For future use with hiera
+  #$password = hiera("password")
+  #$enable = hiera("enable")
+
+  #Zebra variables
+  $zebra_logfile = '/var/log/quagga/zebra.log'
+  
+  #BGP variables
+  $AS = ''
+  $bgp_alwayscomparemed = false
+  $bgp_logfile = '/var/log/quagga/bgpd.log'
+  #$bgp_neighbors is an array in the format 
+  # ISP1 => { 'neighbor_ip' => '192.0.2.1', 'neighbor_as' => '65001' }
+  $bgp_neighbors = {
+    neighbor1 => { 'neighbor_ip' => '', 'neighbor_as' => '' },
+  }
+
+  #OSPF variables
+  $ospf_logfile = '/var/log/quagga/ospfd.log'
+  #$ospf_interfaces is an array in the format 
+  $ospf_interfaces = {
+    interface1 => { 'interface' => '', 'area' => ''},
+  }
+
+  #ospf6_interfaces is an array. By default assign it the same values as ospf_interfaces
+  $ospf6_interfaces = $ospf_interfaces
+}
